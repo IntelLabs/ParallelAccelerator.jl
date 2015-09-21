186506636e1573cdcbfa7653e48e6b3daa408154
Ehsan Totoni <ehsan.totoni@intel.com>
2015-09-21 10:33:26 -0700

    fix bitarray support in domain-ir

--- src/domain-ir.jl
+++ src/domain-ir.jl
@@ -1120,26 +1120,26 @@ function translate_call(state, env, typ, head, oldfun, oldargs, fun, args)
       args = normalize_args(state, env_, args)
       lhs = args[1]
       ranges = is(fun, :getindex) ? args[2:end] : args[3:end]
-      typ = typeOfOpr(state, lhs)
-      if any(Bool[ isrange(typeOfOpr(state, range)) for range in ranges])
+      atyp = typeOfOpr(state, lhs)
+      if any(Bool[ isrange(typeOfOpr(state, range)) || isbitarray(typeOfOpr(state, range)) for range in ranges])
         dprintln(env, "got :getindex with ", args)
         dprintln(env, "ranges is ", ranges)
-        newsize = addGenSym(Int, state.linfo)
-        newlhs = addGenSym(typ, state.linfo)
-        etyp = elmTypOf(typ)
+        #newsize = addGenSym(Int, state.linfo)
+        #newlhs = addGenSym(atyp, state.linfo)
+        etyp = elmTypOf(atyp)
         ranges = mk_ranges([rangeToMask(state, range) for range in ranges]...)
         if is(fun, :getindex) 
           expr = mk_mmap([mk_select(lhs, ranges)], DomainLambda(Type[etyp], Type[etyp], as -> [Expr(:tuple, as...)], LambdaInfo())) 
         else
            args = Any[mk_select(lhs, ranges), args[2]]
-           typs = Type[typ, typeOfOpr(state, args[2])]
+           typs = Type[atyp, typeOfOpr(state, args[2])]
            (nonarrays, args, typs, f) = specialize(state, args, typs, as -> [Expr(:tuple, as[2])])
            elmtyps = Type[ (isarray(t) || isbitarray(t)) ? elmTypOf(t) : t for t in typs ]
            linfo = LambdaInfo()
            for i=1:length(nonarrays)
              # At this point, they are either symbol nodes, or constants
              if isa(nonarrays[i], SymbolNode)
-               addEscapingVariable(nonarrays[i].name, nonarrays[i].typ, 0, linfo)
+               addEscapingVariable(nonarrays[i].name, nonarrays[i].atyp, 0, linfo)
              end
            end
            expr = mk_mmap!(args, DomainLambda(elmtyps, Type[etyp], f, linfo))
