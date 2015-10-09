322fc2723f3f20c9b91ba1c52bd0b4d300cb05cc
Lindsey Kuper <lindsey@composition.al>
2015-10-09 13:29:21 -0700

    Revert "more cgen ASCII"
    This reverts commit 075052089ff17d34908bf413f52efa0c17630b74.

--- src/cgen.jl
+++ src/cgen.jl
@@ -1827,7 +1827,7 @@ function from_root(ast::Expr, functionName::ASCIIString, isEntryPoint = true)
 		bod = from_varargpack(vararglist) * bod
 	end
 
-	hdr::ASCIIString = ""
+	hdr = ""
 	wrapper = ""
 	if istupletyp(returnType)
 	    returnType = tuple(returnType.parameters...)
@@ -1860,7 +1860,7 @@ function from_root(ast::Expr, functionName::ASCIIString, isEntryPoint = true)
 		inEntryPoint = false
 	end
 	push!(lstate.compiledfunctions, functionName)
-	c::ASCIIString = hdr * forwarddecl * from_worklist() * s * wrapper
+	c = hdr * forwarddecl * from_worklist() * s * wrapper
 	if isEntryPoint
 		resetLambdaState(lstate)
 	end	
