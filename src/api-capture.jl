afa359ebb13c6764359a31680ee801e5fa43f749
Leonard Truong <leonardtruong@gmail.com>
2015-10-16 09:27:36 -0700

    Extend api-capture to properly handle augassign nodes

--- src/api-capture.jl
+++ src/api-capture.jl
@@ -4,6 +4,13 @@ import CompilerTools
 import ..API
 import ..operators
 
+ref_assign_map = Dict{Symbol, Symbol}(
+    :(+=) => :(+),
+    :(-=) => :(-),
+    :(*=) => :(*),
+    :(/=) => :(/)
+)
+
 @doc """
 At macro level, we translate function calls and operators that matches operator names
 in our API module to direct call to those in the API module. 
@@ -34,6 +41,20 @@ function process_node(node, state, top_level_number, is_top_level, read)
            Expr(:(=), tmpvar, rhs),
            Expr(:call, GlobalRef(API, :setindex!), lhs, tmpvar, idx...),
            tmpvar]
+    elseif haskey(ref_assign_map, head) && isa(node.args[1], Expr) && node.args[1].head == :ref
+      # x[...] += ...
+      lhs = node.args[1].args[1]
+      idx = node.args[1].args[2:end]
+      rhs = node.args[2]
+      tmpvar = gensym()
+      node.head = :block
+      node.args = Any[
+           Expr(:(=), tmpvar, Expr(:call, ref_assign_map[head],
+                                          Expr(:call, GlobalRef(API, :getindex), lhs, idx...),
+                                          rhs)),
+           Expr(:call, GlobalRef(API, :setindex!), lhs, tmpvar, idx...),
+           tmpvar]
+
     elseif node.head == :ref
       node.head = :call
       node.args = Any[GlobalRef(API, :getindex), node.args...]
