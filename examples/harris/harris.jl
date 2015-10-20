8543f43bf2793fd54f7204b9dd172709ecd20160
Ehsan Totoni <ehsan.totoni@intel.com>
2015-10-20 11:57:03 -0700

    examples replace importall with using

--- examples/harris/harris.jl
+++ examples/harris/harris.jl
@@ -23,7 +23,7 @@ ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
 THE POSSIBILITY OF SUCH DAMAGE.
 =#
 
-importall ParallelAccelerator
+using ParallelAccelerator
 using DocOpt
 using Images
 
