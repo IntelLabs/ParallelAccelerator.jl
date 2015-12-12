d69f57882b1835474062e20a9ab9febef1b903d4
Chunling <chunling.hu@intel.com>
2015-12-11 16:06:51 -0800

    test cases for bug report

---
+++ test/test_sum_maximum.jl
@@ -0,0 +1,20 @@
+importall ParallelAccelerator
+
+@acc function find_chg(k,m,W,Wp)	 
+	W_tmp = [abs(W[i,j] - Wp[i,j]) for i in 1:m, j in 1:k]
+	s = [sum(W_tmp[:,j]) for j in 1:k]
+	chg = maximum(s)
+end
+
+function main(m::Int, k::Int)
+    W  = Array(Float64, m, k)   
+    Wp = Array(Float64, m, k)
+    fill!(W, 3)
+    fill!(Wp, 5)
+    chg = find_chg(k,m,W,Wp)
+
+    println("chg = ", chg)
+
+end
+
+main(100, 100)
