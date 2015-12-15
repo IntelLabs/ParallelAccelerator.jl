29baa9d7bb27f67d4093d0ca9cfe6352d26b4c9a
Paul H. Liu <hai.liu@intel.com>
2015-12-14 16:16:09 -0800

    Fix arith simplifier to deal with Core.Intrinsics only

--- src/domain-ir.jl
+++ src/domain-ir.jl
@@ -662,7 +662,7 @@ function simplify(state, expr)
     return expr
 end
 
-isTopNodeOrGlobalRef(x,s) = is(x, TopNode(s)) || is(x, GlobalRef(Base, s))
+isTopNodeOrGlobalRef(x,s) = is(x, TopNode(s)) || is(x, GlobalRef(Core.Intrinsics, s))
 add_expr(x,y) = y == 0 ? x : mk_expr(Int, :call, TopNode(:checked_sadd), x, y)
 mul_expr(x,y) = y == 0 ? 0 : (y == 1 ? x : mk_expr(Int, :call, TopNode(:checked_smul), x, y))
 neg_expr(x)   = mk_expr(Int, :call, TopNode(:neg_int), x)
