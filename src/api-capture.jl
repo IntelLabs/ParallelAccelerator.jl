903854886887b92a21ae55b28dd926dffb207250
Ehsan Totoni <ehsan.totoni@intel.com>
2015-12-20 20:56:29 -0800

    capture distributed data sources macro level

--- src/api-capture.jl
+++ src/api-capture.jl
@@ -1,8 +1,12 @@
 module Capture
 
+#using Debug
+
 import CompilerTools
 import ..API
 import ..operators
+
+#data_source_num = 0
 
 ref_assign_map = Dict{Symbol, Symbol}(
     :(+=) => :(+),
@@ -29,6 +33,40 @@ function process_node(node::Expr, state, top_level_number, is_top_level, read)
         if isa(opr, Symbol) && in(opr, operators)
             node.args[1] = GlobalRef(API, opr)
         end
+    elseif head == :(=) && isa(node.args[1], Symbol) && isa(node.args[2], Expr) && node.args[2].head ==:call && node.args[2].args[1]==:DataSource
+        arr_var_expr = node.args[2].args[2]
+        dims = arr_var_expr.args[3]
+        @assert arr_var_expr.args[1]==:Array "Data sources need arrays as type"
+        node.args[1] = :($(node.args[1])::$arr_var_expr)
+        source_typ = node.args[2].args[3]
+        @assert source_typ==:HDF5 "Only HDF5 data sources supported for now."
+        hdf_var_name = node.args[2].args[4]
+        hdf_file_name = node.args[2].args[5]
+
+        call_name = symbol("__hps_data_source_$source_typ")
+        call = :($(call_name)($hdf_var_name,$hdf_file_name))
+        node.args[2] = call
+        #@bp
+#=        arr_var_expr = node.args[2].args[2]
+        dims = arr_var_expr.args[3]
+        @assert arr_var_expr.args[1]==:Array "Data sources need arrays as type"
+        
+        source_typ = node.args[2].args[3]
+        @assert source_typ==:HDF5 "Only HDF5 data sources supported for now."
+        
+        hdf_var_name = node.args[2].args[4]
+        hdf_file_name = node.args[2].args[5]
+
+        # return :($(node.args[1]) = zeros($(arr_var_expr.args[2]),$(arr_var_expr.args[3])))
+        num = get_unique_data_source_num()
+        hps_source_var = symbol("__hps_data_source_$num")
+        hps_source_size_var = symbol("__hps_data_source_size_$num")
+        hps_source_size_call = symbol("__hps_data_source_get_size_$(dims)d")
+        declare_expr = :( $hps_source_var = __hps_data_source_open($hdf_var_name,$hdf_file_name))
+        size_expr = :( $hps_source_size_var = $hps_source_size_call($hps_source_var))
+        return [declare_expr; size_expr]
+=#
+
     elseif head == :(=) && isa(node.args[1], Expr) && node.args[1].head == :ref
         # x[...] = ...
         lhs = node.args[1].args[1]
@@ -72,6 +110,12 @@ end
 function process_node(node::Any, state, top_level_number, is_top_level, read)
     CompilerTools.AstWalker.ASTWALK_RECURSE
 end
+
+#function get_unique_data_source_num()
+#    global data_source_num
+#    data_source_num += 1
+#    return data_source_num
+#end
 
 end # module Capture
 
