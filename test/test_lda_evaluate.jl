25283c9372512ab6169f68a021bf4d37bfb27cd5
Chunling <chunling.hu@intel.com>
2015-12-22 12:08:46 -0800

    Added some minor tests from JuliaStats. All of them failed in compilation and are currently excluded from runtest

---
+++ test/test_lda_evaluate.jl
@@ -0,0 +1,26 @@
+using ParallelAccelerator
+
+type newType
+    w::Vector{Float64}
+    b::Float64
+end
+@acc function evaluate(f::newType, X::AbstractMatrix)
+    if f.b != 0
+	X = X + f.b
+    end
+    return X
+end
+
+function main(m::Int, n::Int)
+    X = Array(Float64, m, n)
+    W = Array(Float64, m)
+    fill!(X, 5)
+    fill!(W, 3)
+    b = 1.0
+    f = newType(W,b)
+    evaluate(f, X)
+    println("done")
+
+end
+
+main(100, 200)
---
+++ test/test_norm_w.jl
@@ -0,0 +1,25 @@
+importall ParallelAccelerator
+
+@acc function normalizeW(W::DenseMatrix{Float64}, k::Int)
+# normalize each column
+#=
+    for j = 1:k
+        w = view(W,:,j)
+        scale!(w, 1.0 / sqrt(sumabs2(w)))
+	#s = sqrt(sumabs2(W[:,j]))
+	#W[:,j] /= s
+    end
+=#
+    s = [sqrt(sumabs2(W[:,j])) for j in 1:k]
+    scale!(W,s)
+end
+
+function main(m::Int, k::Int, n::Int)
+    W  = Array(Float64, m, k)   
+    fill!(W, 3)
+    normalizeW(W, k)
+    println("done")
+
+end
+
+main(100, 100, 200)
