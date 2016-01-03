1be88a61deab9a16c6aa0878a5d42cc7a957ffc3
Ehsan Totoni <ehsan.totoni@intel.com>
2016-01-03 11:36:58 -0800

    CGen initialize reductions without OMP bug fix

--- src/cgen.jl
+++ src/cgen.jl
@@ -2139,7 +2139,20 @@ function from_parforstart_serial(args)
     stops = map((a)->from_expr(a.upper), lpNests)
     steps = map((a)->from_expr(a.step), lpNests)
 
-    return "{ {\n"*from_loopnest(ivs, starts, stops, steps)
+    # Generate reduction initializers
+    rds = parfor.reductions
+    rdvars = rdinis = ""
+    dprintln(3,"reductions = ", rds);
+    if !isempty(rds)
+        rdvars = map((a)->from_expr(a.reductionVar), rds)
+        rdinis = map((a)->from_expr(a.reductionVarInit), rds)
+    end
+    rdsprolog = ""
+    for i in 1:length(rds)
+        rdsprolog *= "$(rdvars[i]) = $(rdinis[i]);\n"
+    end
+
+    return rdsprolog*"\n{ {\n"*from_loopnest(ivs, starts, stops, steps)
 
 #  s *= "{\n{\n" * mapfoldl(
 #           (i) -> "for ( $(ivs[i]) = $(starts[i]); $(ivs[i]) <= $(stops[i]); $(ivs[i]) += $(steps[i])) {\n",
