e7f7929cade5cca81dde08ba3666f89c79788b44
Ehsan Totoni <ehsan.totoni@intel.com>
2016-01-05 16:17:36 -0800

    refactor API capture

--- src/api-capture.jl
+++ src/api-capture.jl
@@ -33,8 +33,61 @@ function process_node(node::Expr, state, top_level_number, is_top_level, read)
     elseif head == :call
         # f(...)
         opr = node.args[1]
-        handle_operator(opr, node)
-    elseif head == :(=) && isa(node.args[1], Symbol) && isa(node.args[2], Expr) && node.args[2].head ==:call && node.args[2].args[1]==:DataSource
+        process_operator(node, opr)
+    elseif head == :(=) 
+        process_assignment(node, node.args[1], node.args[2])
+    elseif haskey(ref_assign_map, head) && isa(node.args[1], Expr) && node.args[1].head == :ref
+        # x[...] += ...
+        lhs = node.args[1].args[1]
+        idx = node.args[1].args[2:end]
+        rhs = node.args[2]
+        tmpvar = gensym()
+        node.head = :block
+        node.args = Any[
+        Expr(:(=), tmpvar, Expr(:call, GlobalRef(API, ref_assign_map[head]),
+        Expr(:call, GlobalRef(API, :getindex), lhs, idx...),
+        rhs)),
+        Expr(:call, GlobalRef(API, :setindex!), lhs, tmpvar, idx...),
+        tmpvar]
+
+    elseif haskey(ref_assign_map, head) 
+        # x += ...
+        lhs = node.args[1]
+        rhs = node.args[2]
+        tmpvar = gensym()
+        node.head = :(=)
+        node.args = Any[ lhs, Expr(:call, GlobalRef(API, ref_assign_map[head]), lhs, rhs) ]
+    elseif node.head == :ref
+        node.head = :call
+        node.args = Any[GlobalRef(API, :getindex), node.args...]
+    end
+
+    CompilerTools.AstWalker.ASTWALK_RECURSE
+end
+
+function process_node(node::Any, state, top_level_number, is_top_level, read)
+    CompilerTools.AstWalker.ASTWALK_RECURSE
+end
+
+
+function process_operator(node::Expr, opr::Symbol)
+    api_opr = GlobalRef(API, opr)
+    if in(opr, operators)
+        node.args[1] = api_opr
+    end
+    if in(opr, binary_operator_set) && length(node.args) > 3
+        # we'll turn multiple arguments into pairwise
+        expr = foldl((a, b) -> Expr(:call, api_opr, a, b), node.args[2:end])
+        node.args = expr.args
+    end
+end
+
+
+function process_operator(node::Expr, opr::Any)
+end
+
+function process_assignment(node, lhs::Symbol, rhs::Expr)
+    if rhs.head ==:call && rhs.args[1]==:DataSource
         arr_var_expr = node.args[2].args[2]
         
         @assert arr_var_expr.args[1]==:Array || arr_var_expr.args[1]==:Matrix "Data sources need Array or Matrix as type"
@@ -71,8 +124,16 @@ function process_node(node::Expr, state, top_level_number, is_top_level, read)
         size_expr = :( $hps_source_size_var = $hps_source_size_call($hps_source_var))
         return [declare_expr; size_expr]
 =#
+   elseif rhs.head==:call && isa(rhs.args[1],Expr) && rhs.args[1].head==:. && rhs.args[1].args[1]==:HPS
+        hps_call = rhs.args[1].args[2]
+        new_opr = symbol("__hps_$hps_call")
+        node.args[2].args[1] = new_opr
+        node.args[1] = :($lhs::Matrix{Float64})
+   end
+end
 
-    elseif head == :(=) && isa(node.args[1], Expr) && node.args[1].head == :ref
+function process_assignment(node, lhs::Expr, rhs::Any)
+    if lhs.head == :ref
         # x[...] = ...
         lhs = node.args[1].args[1]
         idx = node.args[1].args[2:end]
@@ -83,61 +144,10 @@ function process_node(node::Expr, state, top_level_number, is_top_level, read)
         Expr(:(=), tmpvar, rhs),
         Expr(:call, GlobalRef(API, :setindex!), lhs, tmpvar, idx...),
         tmpvar]
-    elseif haskey(ref_assign_map, head) && isa(node.args[1], Expr) && node.args[1].head == :ref
-        # x[...] += ...
-        lhs = node.args[1].args[1]
-        idx = node.args[1].args[2:end]
-        rhs = node.args[2]
-        tmpvar = gensym()
-        node.head = :block
-        node.args = Any[
-        Expr(:(=), tmpvar, Expr(:call, GlobalRef(API, ref_assign_map[head]),
-        Expr(:call, GlobalRef(API, :getindex), lhs, idx...),
-        rhs)),
-        Expr(:call, GlobalRef(API, :setindex!), lhs, tmpvar, idx...),
-        tmpvar]
-
-    elseif haskey(ref_assign_map, head) 
-        # x += ...
-        lhs = node.args[1]
-        rhs = node.args[2]
-        tmpvar = gensym()
-        node.head = :(=)
-        node.args = Any[ lhs, Expr(:call, GlobalRef(API, ref_assign_map[head]), lhs, rhs) ]
-    elseif node.head == :ref
-        node.head = :call
-        node.args = Any[GlobalRef(API, :getindex), node.args...]
-    end
-
-    CompilerTools.AstWalker.ASTWALK_RECURSE
-end
-
-function process_node(node::Any, state, top_level_number, is_top_level, read)
-    CompilerTools.AstWalker.ASTWALK_RECURSE
-end
-
-
-function handle_operator(opr::Symbol, node::Expr)
-    api_opr = GlobalRef(API, opr)
-    if in(opr, operators)
-        node.args[1] = api_opr
-    end
-    if in(opr, binary_operator_set) && length(node.args) > 3
-        # we'll turn multiple arguments into pairwise
-        expr = foldl((a, b) -> Expr(:call, api_opr, a, b), node.args[2:end])
-        node.args = expr.args
     end
 end
 
-function handle_operator(opr::Expr, node::Expr)
-    if opr.head==:. && opr.args[1]==:HPS
-        hps_call = opr.args[2]
-        new_opr = symbol("__hps_$hps_call")
-        node.args[1] = new_opr
-    end
-end
-
-function handle_operator(opr::Any, node::Expr)
+function process_assignment(node, lhs::Any, rhs::Any)
 end
 
 #function get_unique_data_source_num()
