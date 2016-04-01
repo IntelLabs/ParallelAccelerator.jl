3822e3a64fb84f0afe7d5a97b6353a3f8191d67d
Ehsan Totoni <ehsan.totoni@intel.com>
2016-04-01 16:29:21 -0700

    try to trigger HPAT build automatically

---
+++ trigger-build.js
@@ -0,0 +1,33 @@
+
+var Travis = require('travis-ci');
+
+var repo = "IntelLabs/HPAT.jl";
+
+var travis = new Travis({
+	version: '2.0.0'
+});
+
+travis.authenticate({
+
+	github_token: process.env.GH_TOKEN
+
+}, function (err, res) {
+	if (err) {
+		return console.error(err);
+	}
+
+	travis.repos(repo.split('/')[0], repo.split('/')[1]).builds.get(function (err, res) {
+		if (err) {
+			return console.error(err);
+		}
+
+		travis.requests.post({
+			build_id: res.builds[0].id
+		}, function (err, res) {
+			if (err) {
+				return console.error(err);
+			}
+			console.log(res.flash[0].notice);
+		});
+	});
+});
