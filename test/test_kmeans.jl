afdb09ecff4e31f662fe511619a2a801c1aacf6e
Ehsan Totoni <ehsan.totoni@intel.com>
2016-09-01 19:33:03 -0700

    add concrete kmeans test

---
+++ test/test_kmeans.jl
@@ -0,0 +1,56 @@
+#=
+Copyright (c) 2015, Intel Corporation
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without 
+modification, are permitted provided that the following conditions are met:
+- Redistributions of source code must retain the above copyright notice, 
+  this list of conditions and the following disclaimer.
+- Redistributions in binary form must reproduce the above copyright notice, 
+  this list of conditions and the following disclaimer in the documentation 
+  and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE 
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
+THE POSSIBILITY OF SUCH DAMAGE.
+=#
+
+module TestKmeans
+using ParallelAccelerator
+
+#ParallelAccelerator.set_debug_level(3)
+#ParallelAccelerator.DomainIR.set_debug_level(3)
+#ParallelAccelerator.CGen.set_debug_level(3)
+
+    
+@acc function kmeans(iterations::Int64)
+    D = 3  # Number of dimensions
+    N = 4
+    numCenter = 2
+    # synthetic but deterministic initial values
+    points = [1. 2. 3. 4.; 4. 3. 2. 1.; 4. 5. 6. 7.]
+    centroids = [1. 3.; 4. 4.; 5. 5.]
+    for l in 1:iterations
+        dist::Array{Array{Float64,1},1} = [ [sqrt(sum((points[:,i]-centroids[:,j]).^2)) for j in 1:numCenter] for i in 1:N]
+        labels::Array{Int,1} = [indmin(dist[i]) for i in 1:N]
+        centroids::Array{Float64,2} = [ sum(points[j,labels.==i])/sum(labels.==i) for j in 1:D, i in 1:numCenter]
+    end 
+    return centroids
+end
+
+end
+
+using Base.Test
+println("Testing kmeans...")
+@test_approx_eq TestKmeans.kmeans(10) [1.5  3.5; 3.5  1.5; 4.5  6.5]
+println("Done testing kmeans...")
+
+
