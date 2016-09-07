a6e054112074b4751aec7a964720bba701db67f7
Ehsan Totoni <ehsan.totoni@intel.com>
2016-09-07 15:39:55 -0700

    add transpose test

--- test/transpose_test.jl
+++ test/transpose_test.jl
@@ -9,10 +9,18 @@ function test()
     return B
 end
 
+@acc function test2()
+    A = [1 2 3; 4 5 6]
+    B = transpose(A)
+    return B
+end
+
+
 end
 
 using Base.Test
 println("testing transpose...")
 @test_approx_eq TestTranspose.test() [1. 4.; 2. 5.; 3. 6.]
+@test TestTranspose.test2()==[1 4; 2 5; 3 6]
 println("Done testing transpose.")
 
