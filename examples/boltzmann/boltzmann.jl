335e447e7a3af2bf9239592d3654e8563dca3d28
Lindsey Kuper <lindsey.kuper@intel.com>
2016-11-29 17:43:38 -0800

    boltzmann: minor cleanups, comments, reconcile w/ plain Julia impl.

--- examples/boltzmann/boltzmann.jl
+++ examples/boltzmann/boltzmann.jl
@@ -39,22 +39,23 @@ end
     avu = 1.
     prevavu = 1.
     ts = 0
+    deltaU = 1e-7
+    t1 = 4/9
+    t2 = 1/9
+    t3 = 1/36
+    c_squ = 1/3
+    omega = 1.0
     while (ts<4000 && 1e-10<abs((prevavu-avu)/avu)) || ts<100
         ux_sum = 0.
-        runStencil(F[1], F[2], F[3], F[4], F[5], F[6], F[7], F[8], F[9], 
+        runStencil(F[1], F[2], F[3], F[4], F[5], F[6], F[7], F[8], F[9],
                    G[1], G[2], G[3], G[4], G[5], G[6], G[7], G[8], G[9], ON, UX, X1,
                    1, :oob_wraparound) do F1, F2, F3, F4, F5, F6, F7, F8, F9, G1, G2, G3, G4, G5, G6, G7, G8, G9, ON, UX, X1
-                       deltaU = 1e-7
-                       t1 = 4/9
-                       t2 = 1/9
-                       t3 = 1/36
-                       c_squ = 1/3
-                       omega = 1.0
+                       # Propagate
                        f4 = F4[1, -1]
                        f3 = F3[0,-1]
                        f2 = F2[-1,-1]
                        f5 = F5[1,0]
-                       f1 = F1[-1,0] 
+                       f1 = F1[-1,0]
                        f6 = F6[1,1]
                        f7 = F7[0,1]
                        f8 = F8[-1,1]
@@ -63,8 +64,8 @@ end
                        ux = (f1 + f2 + f8 - f4 - f5 - f6) / density
                        uy = (f2 + f3 + f4 - f6 - f7 - f8) / density
                        if X1[0,0] == 1
-                           ux = ux + deltaU 
-                       end 
+                           ux = ux + deltaU # Increase inlet pressure
+                       end
                        if ON[0,0] == 1
                            ux = 0.
                            uy = 0.
@@ -76,11 +77,14 @@ end
                        u_c4  = uy - ux
                        u_c6  = - u_c2
                        u_c8  = - u_c4
+                       # Calculate equilibrium distribution: stationary
                        feq9 = t1 * density * (1. - u_squ / (2. * c_squ))
+                       # nearest-neighbours
                        feq1 = computeF(density, c_squ, u_squ, t2, ux, ux)
                        feq3 = computeF(density, c_squ, u_squ, t2, uy, uy)
                        feq5 = computeF(density, c_squ, u_squ, t2, -ux, ux)
                        feq7 = computeF(density, c_squ, u_squ, t2, -uy, uy)
+                       # next-nearest neighbours
                        feq2 = computeF(density, c_squ, u_squ, t3, u_c2, u_c2)
                        feq4 = computeF(density, c_squ, u_squ, t3, u_c4, u_c4)
                        feq6 = computeF(density, c_squ, u_squ, t3, u_c6, u_c6)
@@ -124,7 +128,7 @@ function boltzmann(nx, ny)
     X1 = [ x == 1 ? 1 : 0 for x = 1:nx, y = 1:ny ]
     UX = zeros(nx, ny)
     ON = zeros(Int, nx, ny)
-    ON[rand(nx,ny) .> 0.7] = 1
+    ON[rand(nx, ny) .> 0.7] = 1 # extremely porous random domain
     numactivenodes = sum(1 .- ON)
     boltzmann_kernel(numactivenodes, F, G, ON, UX, X1)
 end
@@ -152,7 +156,7 @@ Options:
     println("grid size: ", nx, " x ", ny)
 
     tic()
-    F = boltzmann(0,0)
+    F = boltzmann(1, 1)
     println("SELFPRIMED ", toq())
 
     tic()
