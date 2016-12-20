4c0ed4e1373fc908ddcc39659cc8dd5ceaaa1f4b
Ehsan Totoni <ehsan.totoni@intel.com>
2016-12-20 10:58:41 -0800

    flatten include pre and post parfor statements

--- src/parallel-ir-flatten.jl
+++ src/parallel-ir-flatten.jl
@@ -2,27 +2,27 @@
 Copyright (c) 2015, Intel Corporation
 All rights reserved.
 
-Redistribution and use in source and binary forms, with or without 
+Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
-- Redistributions of source code must retain the above copyright notice, 
+- Redistributions of source code must retain the above copyright notice,
   this list of conditions and the following disclaimer.
-- Redistributions in binary form must reproduce the above copyright notice, 
-  this list of conditions and the following disclaimer in the documentation 
+- Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
   and/or other materials provided with the distribution.
 
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE 
-LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
-CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
-SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
 INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
-ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
 THE POSSIBILITY OF SUCH DAMAGE.
-=# 
- 
+=#
+
 function flattenParfors(function_name, ast)
     #assert(isfunctionhead(ast))
     flatten_start = time_ns()
@@ -33,7 +33,7 @@ function flattenParfors(function_name, ast)
     #s = sprint(io->Base.show_backtrace(io, bt))
     #@dprintln(3, "from_root backtrace ")
     #@dprintln(3, s)
- 
+
     start_time = time_ns()
 
     if isa(ast, Tuple)
@@ -108,7 +108,7 @@ end
 
 """
 Takes a new array of body statements in the process of construction in "new_body" and takes a parfor to add to that
-body.  This parfor is in the nested (parfor code is in the parfor node itself) temporary form we use for fusion although 
+body.  This parfor is in the nested (parfor code is in the parfor node itself) temporary form we use for fusion although
 pre-statements and post-statements are already elevated by this point.  We replace this nested form with a non-nested
 form where we have a parfor_start and parfor_end to delineate the parfor code.
 """
@@ -117,12 +117,20 @@ function flattenParfor(new_body, the_parfor :: ParallelAccelerator.ParallelIR.PI
 
     private_set = getPrivateSet(the_parfor.body, linfo)
     private_array = collect(private_set)
-
+    # append pre-statements
+    append!(new_body, the_parfor.preParFor)
+    #if length(the_parfor.preParFor)!=0
+    #    println("PREPARFOR ",the_parfor.preParFor)
+    #end
     # Output to the new body that this is the start of a parfor.
     push!(new_body, TypedExpr(Int64, :parfor_start, PIRParForStartEnd(the_parfor.loopNests, the_parfor.reductions, the_parfor.instruction_count_expr, private_array)))
     # Output the body of the parfor as top-level statements in the new function body and convert any other parfors we may find.
     flattenParfors(new_body, the_parfor.body, linfo)
     # Output to the new body that this is the end of a parfor.
     push!(new_body, TypedExpr(Int64, :parfor_end, PIRParForStartEnd(deepcopy(the_parfor.loopNests), deepcopy(the_parfor.reductions), deepcopy(the_parfor.instruction_count_expr), deepcopy(private_array))))
+    append!(new_body, the_parfor.postParFor)
+    #if length(the_parfor.postParFor)!=0
+    #    println("POSTPARFOR ",the_parfor.postParFor)
+    #end
     nothing
 end
