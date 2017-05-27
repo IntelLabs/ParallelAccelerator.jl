d2734479fa74a2d1b1ee1704b8c0f6d47abf5dbe
Todd A. Anderson <todd.a.anderson@intel.com>
2017-05-26 17:24:08 -0700

    Fix.

--- examples/pi/pi.jl
+++ examples/pi/pi.jl
@@ -33,7 +33,7 @@ using DocOpt
 @acc function calcPi(n)
     x = rand(n) * 2.0 - 1.0
     y = rand(n) * 2.0 - 1.0
-    return 4.0*sum(x^2 + y^2 < 1.0)/n
+    return 4.0*sum(x.^2 + y.^2 .< 1.0)/n
 end
 
 
